e7d309e72122227595c00e81d84f133246cbe6ce
DaniilSeleznyov <danil_king@mail.ru>
2021-01-14 20:38:48 +0500

    Выполнил задание 1

---
+++ lesson1-1.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    # 1 задание
+    name = input("Введите ваше имя: ")
+    surname = input("Введите вашу фамилию: ")
+    age = int(input("Введите ваш возраст: "))
+    print('Данные пользователя: ', name, surname, str(age) + ' лет')
